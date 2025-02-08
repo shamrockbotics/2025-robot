c6f4c6e747c299425c18058d3cf77fea4df4f3ad
SHAM-ROCK-BOTICS <shamrockbotics@gmail.com>
2025-02-08 10:15:59 -0500

    Add ArmIOSparkRIO class

--- src/main/java/frc/robot/subsystems/arm/JellybeanArmConfig.java
+++ src/main/java/frc/robot/subsystems/arm/JellybeanArmConfig.java
@@ -14,17 +14,8 @@ public class JellybeanArmConfig extends ArmConfig {
     allowedErrorRads = Units.degreesToRadians(2);
     if (real) {
       io =
-          new ArmIOSpark(
-              15,
-              13,
-              Units.degreesToRadians(0),
-              false,
-              false,
-              2 * Math.PI,
-              2 * Math.PI / 60,
-              40,
-              2.0,
-              0.0);
+          new ArmIOSparkRIO(
+              15, 13, 0, 1.2, false, false, 2 * Math.PI, 2 * Math.PI / 60, 40, 2.0, 0.0);
     } else {
       io = new ArmIOSim(minAngleRads, maxAngleRads, 200, (2.0 * Math.PI / 4096), 1.0, 1.0);
     }
