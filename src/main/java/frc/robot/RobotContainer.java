8cd8cda05ffa3b17be988432a47cd27a266344fa
SHAM-ROCK-BOTICS <shamrockbotics@gmail.com>
2025-02-21 20:38:54 -0500

    Latest code for vision

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -67,7 +67,8 @@ public class RobotContainer {
             new Vision(
                 drive::addVisionMeasurement,
                 new VisionIOPhotonVision(camera0Name, robotToCamera0),
-                new VisionIOPhotonVision(camera1Name, robotToCamera1));
+                new VisionIOPhotonVision(camera1Name, robotToCamera1),
+                new VisionIOPhotonVision(camera2Name, robotToCamera2));
         jellybeanArm = new Arm(new JellybeanArmConfig());
         break;
 
--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -16,21 +16,34 @@ package frc.robot.subsystems.vision;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.util.Units;
 
 public class VisionConstants {
   // AprilTag layout
   public static AprilTagFieldLayout aprilTagLayout;
 
   // Camera names, must match names configured on coprocessor
-  public static String camera0Name = "camera_0";
-  public static String camera1Name = "camera_1";
+  public static String camera0Name = "front_camera";
+  public static String camera1Name = "back_left_camera";
+  public static String camera2Name = "back_right_camera";
 
   // Robot to camera transforms
   // (Not used by Limelight, configure in web UI instead)
   public static Transform3d robotToCamera0 =
-      new Transform3d(0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, 0.0));
+      new Transform3d(
+          Units.inchesToMeters(-2), Units.inchesToMeters(-0.5), 0.6, new Rotation3d(0.0, 0.0, 0.0));
   public static Transform3d robotToCamera1 =
-      new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
+      new Transform3d(
+          Units.inchesToMeters(-6),
+          Units.inchesToMeters(-2.5),
+          0.6,
+          new Rotation3d(0.0, 0.0, -Math.PI * 2 / 3));
+  public static Transform3d robotToCamera2 =
+      new Transform3d(
+          Units.inchesToMeters(-6),
+          Units.inchesToMeters(-1.5),
+          0.6,
+          new Rotation3d(0.0, 0.0, Math.PI * 2 / 3));
 
   // Basic filtering thresholds
   public static double maxAmbiguity = 0.3;
@@ -59,7 +72,7 @@ public class VisionConstants {
     try {
       // Full path to the JSON file
       String jsonFilePath =
-          "src/main/deploy/whshallway1.json"; // Adjust this to the correct path on your system
+          "deploy/whshallway1.json"; // Adjust this to the correct path on your system
 
       aprilTagLayout = new AprilTagFieldLayout(jsonFilePath);
 
