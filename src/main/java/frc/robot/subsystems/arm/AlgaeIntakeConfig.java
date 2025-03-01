d206d4aa81db2804a8ca960f8223bd73dc735e85
Lucas K N Sintim <LKNSintim@outlook.com>
2025-03-01 15:21:59 -0500

    Add seperate config files for each type of motor

---
+++ src/main/java/frc/robot/subsystems/arm/AlgaeIntakeConfig.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems.arm;
+
+import edu.wpi.first.math.util.Units;
+
+public class AlgaeIntakeConfig extends ArmConfig {
+  public AlgaeIntakeConfig() {
+    this(true);
+  }
+
+  public AlgaeIntakeConfig(boolean real) {
+    name = "Algae Intake";
+    minAngleRads = 2;
+    maxAngleRads = 0;
+    allowedErrorRads = Units.degreesToRadians(2);
+    if (real) {
+      io =
+          new ArmIOSparkRIO(
+              15, 13, 0, 1.2, false, false, 2 * Math.PI, 2 * Math.PI / 60, 40, 0.5, 0.0);
+    } else {
+      io = new ArmIOSim(minAngleRads, maxAngleRads, 200, (2.0 * Math.PI / 4096), 1.0, 1.0);
+    }
+  }
+}
