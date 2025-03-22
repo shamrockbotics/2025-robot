69054e43044de776e5e80680799f901ec6ec5d62
SHAM-ROCK-BOTICS <shamrockbotics@gmail.com>
2025-03-21 20:44:57 -0400

    Update at 'Fri Mar 21 20:44:56 EDT 2025'

--- src/main/java/frc/robot/subsystems/arm/AlgaeArmConfig.java
+++ src/main/java/frc/robot/subsystems/arm/AlgaeArmConfig.java
@@ -10,12 +10,12 @@ public class AlgaeArmConfig extends ArmConfig {
   public AlgaeArmConfig(boolean real) {
     name = "Algae Arm";
     minAngleRads = -2.0;
-    maxAngleRads = 0.5;
+    maxAngleRads = 0.4;
     allowedErrorRads = Units.degreesToRadians(2);
     if (real) {
       io =
           new ArmIOSparkMax(
-              15, -2.016, false, true, 2 * Math.PI, 2 * Math.PI / 60, 40, 10.0, 0.1, 0.0);
+              15, -2.016, false, true, 2 * Math.PI, 2 * Math.PI / 60, 40, 10.0, 0.5, 0.1);
     } else {
       io = new ArmIOSim(minAngleRads, maxAngleRads, 200, (2.0 * Math.PI / 4096), 1.0, 1.0);
     }
