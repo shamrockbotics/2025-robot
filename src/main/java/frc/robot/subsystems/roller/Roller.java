404d443a4a87d9422344af093564a50890edce21
Matt Veldman <Matthew.J.Veldman@aptiv.com>
2025-03-25 16:21:18 -0400

    Add roller subsystems to dashboard

--- src/main/java/frc/robot/subsystems/roller/Roller.java
+++ src/main/java/frc/robot/subsystems/roller/Roller.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.roller;
 
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.function.DoubleSupplier;
@@ -23,9 +24,11 @@ public class Roller extends SubsystemBase {
     this.intakePercent = config.intakePercent;
     this.releasePercent = config.releasePercent;
 
-    setDefaultCommand(run(() -> stop()));
-
     disconnectedAlert = new Alert(getName() + " disconnected.", AlertType.kError);
+
+    setDefaultCommand(run(() -> stop()).withName("Stop"));
+
+    SmartDashboard.putData(this);
   }
 
   @Override
