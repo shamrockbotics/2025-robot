c14b83eb0735ef64b338ab8971752446aab6c51a
SHAM-ROCK-BOTICS <shamrockbotics@gmail.com>
2025-04-04 11:55:39 -0400

    Update at 'Fri Apr 04 11:55:38 EDT 2025'

--- src/main/java/frc/robot/subsystems/ElevatorConfig.java
+++ src/main/java/frc/robot/subsystems/ElevatorConfig.java
@@ -27,7 +27,7 @@ public class ElevatorConfig extends MechanismConfig {
                   3.0,
                   0.0)
               .addFollower(9, true)
-              .addFeedforward(new ElevatorFeedforward(0, 1.5, 0));
+              .addFeedforward(new ElevatorFeedforward(0, 1.0, 0));
     } else {
       io = new MechanismIOSim(minPosition, maxPosition, 200, (2.0 * Math.PI / 4096), 1.0, 1.0);
     }
